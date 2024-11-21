874ded1a88c70fd7e2a25df8ab4e504cef8fdf6b
scleeh <00001107225462sp@al.educacao.sp.gov.br>
2024-11-21 11:59:17 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,54 @@
+criaCartao(
+    'Pergunta N°1',
+    'Como Klaus Mikaelson reagiu ao descobrir que seria pai?',
+    'Klaus inicialmente ficou em negação ao saber da gravidez de Hayley, acreditando que sua natureza violenta e os traumas do passado o impediriam de ser um bom pai. Com o tempo, no entanto, ele passou a ver Hope como uma chance de redenção e a amou profundamente.'
+    
+)
+
+criaCartao(
+    'Pergunta N°2',
+    'Como a gravidez de Hope mudou a dinâmica da família Mikaelson?',
+    'Antes do nascimento de Hope, a família era marcada por traições e conflitos. Sua chegada trouxe um novo propósito, unindo os irmãos, especialmente Elijah, que se dedicou a manter a família unida e protegê-la.'
+)
+
+criaCartao(
+    'Pergunta N°3',
+    'Qual era o principal temor de Klaus em relação a Hope?',
+    'Klaus temia que Hope herdasse o lado sombrio e destrutivo que ele possuía. Além disso, ele sabia que ela estaria constantemente em perigo devido aos inúmeros inimigos que a família Mikaelson acumulou ao longo dos séculos.'
+)
+
+criaCartao(
+    'Pergunta N°4',
+    'Como Hope foi vista pelas outras criaturas sobrenaturais?',
+    'Hope foi considerada uma criatura única, sendo a primeira tríbida da história (vampira, lobisomem e bruxa). Isso fez dela tanto um símbolo de poder quanto uma ameaça para muitas facções sobrenaturais, o que colocou sua vida em risco desde o nascimento.  '
+)
+
+criaCartao(
+    'Pergunta N°5',
+    'Quais os maiores desafios enfrentados pelos Mikaelson ao tentar proteger Hope, considerando as ameaças que surgem em Legacies?',
+    'Os Mikaelson enfrentam grandes desafios para proteger Hope. Mesmo com Klaus ausente, seu legado pesa sobre a família. Elijah e Rebekah lutam para garantir um futuro seguro para Hope, sacrificando suas próprias naturezas imortais para protegê-la.'
+)
+
+criaCartao(
+    'Pergunta N°6',
+    'Quem são os membros principais da família Mikaelson e qual a sua origem?',
+    'Os Mikaelson é formada pelos irmãos Elijah, Klaus, Rebekah e, pela filha Hope. São os primeiros vampiros, os "originais". Klaus é o único híbrido (vampiro e lobisomem) devido a uma maldição. Sua história é marcada por tragédias e conflitos com o pai.'
+)
+
+criaCartao(
+    'Pergunta N°7',
+    'Quais são as habilidades especiais de Hope Mikaelson e como elas se manifestam em Legacies?',
+    'Hope Mikaelson, filha de Klaus (híbrido) e Hayley (lobisomem), é uma "Tribrida", com as habilidades de vampiro, lobisomem e bruxa. Em Legacies, ela desenvolve seu controle sobre essas naturezas. Ela se tornam uma personagens poderosas e alvo de muitos.'
+)
+
+criaCartao(
+    'Pergunta N°8',
+    'Por que Klaus Mikaelson é tão temido e odiado pelos outros personagens?',
+    'Klaus é temido devido à sua natureza cruel. Ele cometeu várias atrocidades ao longo dos séculos, o que fez com que fosse visto como um monstro. Sua capacidade de fazer o que for necessário para manter o poder e proteger sua família o torna uma ameaça constante.'
+)
+
+criaCartao(
+    'Pergunta N°9',
+    'Qual foi o maior sacrifício de Hayley por Hope em The Originals?',
+    'O maior sacrifício de Hayley foi sua vida, quando se sacrificou para salvar Hope em *The Originals*. Ela morreu durante um confronto, garantindo a segurança de Hope e o legado dela, simbolizando o amor incondicional de Hayley pela filha.'
+)
